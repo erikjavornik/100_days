b786016815bd18b5e86cefa22cd0b45cd437bda3
Erik Javornik <ej83@njit.edu>
2023-08-20 16:41:16 -0400

    Completed challenges

--- Day_32_Email/Birthday_Wish/birthday_main.py
+++ Day_32_Email/Birthday_Wish/birthday_main.py
@@ -7,7 +7,9 @@ import smtplib, datetime as dt, random, pandas
 
 # 2. Check if today matches a birthday in the birthdays.csv
 # HINT 1: Create a tuple from today's month and day using datetime. e.g.
-# today = (today_month, today_day)
+# today = (today_month, today_day)/Day_32_Email/Birthday_Wish/birthday-wisher-normal-start/letter_templates/
+now = dt.datetime.now()
+today = (now.month, now.day)
 
 # HINT 2: Use pandas to read the birthdays.csv
 df = pandas.read_csv("./Day_32_Email/Birthday_Wish/birthday_data.csv")
@@ -20,22 +22,39 @@ df = pandas.read_csv("./Day_32_Email/Birthday_Wish/birthday_data.csv")
 # new_dict = {new_key: new_value for (index, data_row) in data.iterrows()}
 #e.g. if the birthdays.csv looked like this:
 # name,email,year,month,day
-# Angela,angela@email.com,1995,12,24
-#Then the birthdays_dict should look like this:
+# Angela,angela@email.com,1995,12,24/Day_32_Email/Birthday_Wish/birthday-wisher-normal-start/letter_templates/
 # birthdays_dict = {
 #     (12, 24): Angela,angela@email.com,1995,12,24
 # }
+birthday_dict = {(row.month,row.day):row for (index, row) in df.iterrows()}
 
 #HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
 # if (today_month, today_day) in birthdays_dict:
-
 # 3. If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
 # HINT 1: Think about the relative file path to open each letter. 
 # HINT 2: Use the random module to get a number between 1-3 to pick a randome letter.
 # HINT 3: Use the replace() method to replace [NAME] with the actual name. https://www.w3schools.com/python/ref_string_replace.asp
+if today in birthday_dict:
+    rand_letter = random.randint(1,3)
+    person_name = birthday_dict[today]["name"]
+    print(person_name)
+    with open(f"./Day_32_Email/Birthday_Wish/letter_temp/letter_{rand_letter}.txt") as file:
+        letter = file.read()
+        named_letter = letter.replace("[NAME]", person_name)
 
 # 4. Send the letter generated in step 3 to that person's email address.
 # HINT 1: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
 # HINT 2: Remember to call .starttls()
 # HINT 3: Remember to login to your email service with email/password. Make sure your security setting is set to allow less secure apps.
 # HINT 4: The message should have the Subject: Happy Birthday then after \n\n The Message Body.
+    my_email = "...@gmail.com"
+    password = ""
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.loggin(user=my_email, password= password)
+        connection.sendmail(
+            from_addr=my_email, 
+            to_addrs=birthday_dict[today]["email"], 
+            msg=f"Subject:Happy Birthday\n\n{named_letter}"
+        )
--- Day_33_API/issoverhead-start/iss_overhead_main.py
+++ Day_33_API/issoverhead-start/iss_overhead_main.py
@@ -1,8 +1,8 @@
-import requests
+import requests, smtplib, time
 from datetime import datetime
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = 40.679597 # Your latitude
+MY_LONG = -73.907776 # Your longitude
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -12,26 +12,48 @@ iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
 #Your position is within +5 or -5 degrees of the ISS position.
+def check_position():
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+    else:
+        return False
 
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now()
 
-time_now = datetime.now()
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+    else:
+        return False
 
 #If the ISS is close to my current position
 # and it is currently dark
 # Then send me an email to tell me to look up.
 # BONUS: run the code every 60 seconds.
+my_email = "...@gmail.com"
+password = ""
 
-
+while True:
+    time.sleep(60)
+    if check_position() and is_night():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.loggin(user=my_email, password= password)
+            connection.sendmail(
+                from_addr=my_email, 
+                to_addrs=my_email, 
+                msg="Subject:Look up\n\nThe ISS is above you."
+            )
 
