9994066bc96e658ae80951f2a13760c43a701f7e
Erik Javornik <ej83@njit.edu>
2024-10-24 08:34:37 -0400

    Completed Day 45

--- Intermediate+_(32-58)/Day_45_Beautiful_Soup/Starting Code - 100 movies to watch start/main.py
+++ Intermediate+_(32-58)/Day_45_Beautiful_Soup/Starting Code - 100 movies to watch start/main.py
@@ -5,23 +5,36 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
+# Get the data and text from the URL
 response = requests.get(URL)
-
 web_page = response.text
 
+# Create soup object and get the titles based on the webpages structure
 soup = BeautifulSoup(web_page, "html.parser")
-
 movie_data = soup.find_all(name="h3", class_="title")
 
+# Create a list to hold the text from the h3 title tags
 movie_titles = []
-
 for movie in movie_data:
     title = movie.getText()
+    #Attemp to fix : error for number 12 movie
+    # title_clean = title.replace(":",")")
     movie_titles.append(title)
-    
+
+#Attemp to fix : error for number 12 movie
 # print(movie_titles)
 # movie_titles.sort()
 # print(movie_titles)
+# movie_titles_clean = movie_titles.replace(":",")")
+# movie_number = [int(num.split(')')[0]) for num in movie_titles]
+# [int(num.split()[0]) for num in movie_titles]
+# print(movie_number)
 
-movie_number = []
+# Reverse the order of the list so in numerical order (1,2,3...)
+movie_titles.reverse()
+# print(movie_titles)
 
+# # Write the text file for the movie list. Stop from running every time
+# with open("movies.txt", mode="w", encoding="utf-8") as file:
+#     for movie in movie_titles:
+#         file.write(f"{movie}\n")
